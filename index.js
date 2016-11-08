435b21453d3462a47050f3e6c726e61585148fa1
Fabio <fabio.barbosa@gmail.com>
2016-11-08 19:53:51 -0200

    commit

---
+++ index.js
@@ -0,0 +1,27 @@
+'use strict'
+
+const Telegram = require('telegram-node-bot')
+const TelegramBaseController = Telegram.TelegramBaseController
+const TextCommand = Telegram.TextCommand
+const tg = new Telegram.Telegram('296878881:AAFYwRpPLPPg9_JmyeAhDBdtyaHGEUqiVuc')
+
+class PingController extends TelegramBaseController {
+    /**
+     * @param {Scope} $
+     */
+    pingHandler($) {
+        $.sendMessage('pong')
+    }
+
+    get routes() {
+        return {
+            'pingCommand': 'pingHandler'
+        }
+    }
+}
+
+tg.router
+    .when(
+        new TextCommand('ping', 'pingCommand'),
+        new PingController()
+    )
