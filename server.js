355117a69a742ac9105529069c826fd855d545b5
Fabio <fabio.barbosa@gmail.com>
2016-11-09 00:46:21 -0200

    modificado para enviar a resposta em formato string

--- server.js
+++ server.js
@@ -11,10 +11,15 @@ bot.onText(/\/listar/, function (msg, match) {
   var fromId = msg.from.id;
   console.log('acionado o método listar');
   ckan.exec("package_list", function(err, resp) {
-	  bot.sendMessage(fromId, resp);
+	var resposta = '';
+	for(var x=0;x<resp.result.length;x++){
+		resposta = resposta + resp.result[x] + '<br>';
+	}
+	 
+	bot.sendMessage(fromId, resposta);
   });
 });
-
+ 
 
 
 bot.onText(/\/help/, function(msg, match) {
