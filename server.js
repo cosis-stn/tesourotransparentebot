892864b3ed0ddf2d469834e96afd4adeb1e1a3af
Fabio <fabio.barbosa@gmail.com>
2016-11-09 00:16:14 -0200

    Adicionado o método Listar

--- server.js
+++ server.js
@@ -1,25 +1,22 @@
 var TelegramBot = require('node-telegram-bot-api');
+var ckan = require("node-ckan");
+ckan.setServer("http://www.tesourotransparente.gov.br/ckan");
 
 var token = '296878881:AAFYwRpPLPPg9_JmyeAhDBdtyaHGEUqiVuc';
 // Setup polling way
 var bot = new TelegramBot(token, {polling: true});
 
 // Matches /echo [whatever]
-bot.onText(/\/echo (.+)/, function (msg, match) {
+bot.onText(/\/Listar/, function (msg, match) {
   var fromId = msg.from.id;
-  var resp = match[1];
-  bot.sendMessage(fromId, resp);
+  ckan.exec("package_list", function(err, resp) {
+	  bot.sendMessage(fromId, resp);
+  }  
 });
 
-// Any kind of message
-bot.on('message', function (msg) {
-  var chatId = msg.chat.id;
-  // photo can be: a file path, a stream or a Telegram file_id
-  var photo = 'cats.png';
-  bot.sendPhoto(chatId, photo, {caption: 'Lovely kittens'});
-});
+
 
 bot.onText(/\/help/, function(msg, match) {
   var fromId = msg.from.id;
-  bot.sendMessage(fromId, "Lista de comandos.");
+  bot.sendMessage(fromId, "Lista de comandos disponíveis:<br>/Listar - lista todos os datasets do portal");
 });
